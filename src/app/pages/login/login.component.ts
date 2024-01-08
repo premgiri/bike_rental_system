24fb0c6f34816db6ac43c03af1e35edd16e2f92e
Prem G <gpremgiri1@gmail.com>
2024-01-08 13:56:28 +0530

    Added forgot password functionality and show password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  isShowForgotPassword:boolean = false;
+  isCheckedShowPassword:boolean = false;
+  loginForm!: FormGroup;
+  constructor(private fb:FormBuilder, private loginService:LoginService){
+    this.loginForm = this.fb.group({
+      email:new FormControl(''),
+      password:new FormControl('')
+    })
+  }
+  ngOnInit(){
+    
+  }
+  onLogin(){
+    const payload = {
+      email:this.loginForm.value.email,
+      password:this.loginForm.value.password,
+      returnSecureToken:true
+    }
+    this.loginService.loginwithFirebase(payload).subscribe((response:any)=>{
+      console.log(response);
+    })
+  }
+  onSubmitForgotPassword(){
+    
+  }
 }
--- src/app/pages/login/services/login.service.ts
+++ src/app/pages/login/services/login.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ApiData } from 'src/app/shared/apis';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor() { }
+  private firebaseLoginAPI = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCdctFxPpleBmE2I4jNGt4pe-slEIk62Nc'
+  constructor(private http: HttpClient) { }
+  loginwithFirebase(loginDetails:any){
+    return this.http.post(this.firebaseLoginAPI,loginDetails)
+  }
 }
